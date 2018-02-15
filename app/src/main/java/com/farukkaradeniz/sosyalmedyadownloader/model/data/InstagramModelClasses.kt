5a74239590676dfd2c2f3a70b1c2e8141b3c7438
Faruk Karadeniz <krdnzomer@gmail.com>
2018-02-15 21:39:11 +0300

    Instagram model class added

---
+++ app/src/main/java/com/farukkaradeniz/sosyalmedyadownloader/model/data/InstagramModelClasses.kt
@@ -0,0 +1,16 @@
+package com.farukkaradeniz.sosyalmedyadownloader.model.data
+
+/**
+ * Created by Faruk Karadeniz on 15.02.2018.
+ * Twitter: twitter.com/Omeerfk
+ * Github: github.com/FarukKaradeniz
+ * LinkedIn: linkedin.com/in/FarukKaradeniz
+ * Website: farukkaradeniz.com
+ */
+// regex for instagram html "p\/(\w*)"   group1
+class InstagramPost(
+        val type: String,
+        val previewImageUrl: String,
+        val videoUrl: String?,
+        val duration: String = "--/--"
+)
